b7b3f39a3852ebc03e47ca67f4f77b90a16c76d2
Bobby Zia <mr.baberzia@yahoo.com>
2021-01-01 16:43:52 -0500

    Updates

---
+++ precache-manifest.1d357ce9ee27d5847c14957cede1882b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c88d366243c3ece2e011c80b4825ec8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96168b2ece198193f087",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "f08f5c2faeba83ba75d8",
+    "url": "/static/js/2.3b189aff.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.3b189aff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96168b2ece198193f087",
+    "url": "/static/js/main.b20adea3.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "d3e6cfab95f7fd23ea680099e7839d29",
+    "url": "/static/media/personalpic.d3e6cfab.JPG"
+  },
+  {
+    "revision": "92ffe385ec136511bc3c691a773b81a8",
+    "url": "/static/media/resume.92ffe385.pdf"
+  }
+]);
